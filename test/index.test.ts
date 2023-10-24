c7c9fb3653a0e27839eba442b48ec123fa518004
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-10-24 10:02:54 -0400

    Primeiro teste da aplicação

---
+++ test/index.test.ts
@@ -0,0 +1,13 @@
+import {describe, it, expect} from 'vitest'
+
+describe('Primeiro teste da aplicação', () => {
+  it('should pass', () => {
+    expect(true).toBe(true)
+  })
+})
+
+describe('Cadastro Filme', () => {
+    it('Deve cadastrar um filme com sucesso', () => {
+        expect(true).toBe(true)
+    })
+})
