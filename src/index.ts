d158c95a69c43383b8004c0d36de0d930d53125f
steeh08 <stefanny.rodrigues@estudante.ifms.edu.br>
2023-11-21 08:51:51 -0400

    atv

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import express, {Request} from 'express';
 import BancoMongoDB from './infra/banco/banco-mongodb';
-import ListarFilme from './aplicacao/listar-filme.use-case'
-import SalvarFilme from './aplicacao/salva-filme.use-case'
-const bancoMongoDB = new BancoMongoDB()
+import ListarFilme from './aplicacao/listar-filme.use-case';
+import SalvarFilme from './aplicacao/salva-filme.use-case';
+
+// Cria uma instância do aplicativo Express
+const bancoMongoDB = new BancoMongoDB();
+
+
 const app = express();
 app.use(express.json())
 
 
+
+
+// Define uma rota padrão
 app.get('/filmes', async (req, res) => {
     const listarFilme = new ListarFilme(bancoMongoDB)
     const filmes = await listarFilme.execute()
-    res.status(200).send(filmes)        
+    res.send(filmes).status(200)        
 });
 
-app.post('/filmes', (req, res) => {
-    const salvarFilme = new SalvarFilme(bancoMongoDB)
-    const filmes = salvarFilme
+app.post('/filmes', async (req:Request, res) => {
     const {id, titulo, descricao, foto} = req.body
     const filme:Filme = {
         id,
@@ -23,16 +28,29 @@ app.post('/filmes', (req, res) => {
         descricao,
         foto,
     }
+    const salvarFilme = new SalvarFilme(bancoMongoDB)
+    const filmes = await salvarFilme.execute(filme)
     
-    res.status(201).send(filme)
+    const filmerepetido = filmes_repositorio.find(filme => filme.id === id)
+    if(filmerepetido){
+         return res.status(400).send({error: 'Filme já cadastrado'})
+    }
+
+    filmes_repositorio.push(filme)
+    res.status(201).send(filmes)
 });
 
 app.delete('/filmes/:id', (req, res) => {
     const id = parseInt(req.params.id)
+
     const filme = filmes_repositorio.find(filme => filme.id === id)
+    
     if (!filme) return res.status(404).send(filme)
+
     const filterFilme = filmes_repositorio.filter(filme => filme.id !== id)
+    
     filmes_repositorio = filterFilme
+    
     res.status(200).send(filme)
 });
 
